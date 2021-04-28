1cf33f5ad2fab39ea93228b5cd02d32acbeb6070
jakez <zhu.yifa@husky.neu.edu>
2021-04-29 04:13:36 +0800

    update student_img

--- src/components/videocall/VideoCallStudentGallery.jsx
+++ src/components/videocall/VideoCallStudentGallery.jsx
@@ -3,6 +3,14 @@ import { Grid } from '@material-ui/core';
 
 import VideoCallStudentThumbnail from './VideoCallStudentThumbnail';
 import PlaceholderImage from '../../images/placeholder.png';
+import S1 from '../../images/Student1.png';
+import S2 from '../../images/Student2.png';
+import S3 from '../../images/Student3.png';
+import S4 from '../../images/Student4.png';
+import S5 from '../../images/Student5.png';
+import S6 from '../../images/Student6.png';
+
+import { getByTitle } from '@testing-library/dom';
 
 const VideoCallStudentGallery = ({ setHasClickedPullAside }) => {
   const [pulledAside, setPulledAside] = useState(0);
@@ -15,49 +23,49 @@ const VideoCallStudentGallery = ({ setHasClickedPullAside }) => {
   return (
     <Grid container spacing={1}>
       <VideoCallStudentThumbnail
-        src={PlaceholderImage}
+        src={S1}
         idx={1}
         pulledAside={pulledAside}
         updatePullAside={updatePulledAside}
       />
       <VideoCallStudentThumbnail
-        src={PlaceholderImage}
+        src={S2}
         idx={2}
         pulledAside={pulledAside}
         updatePullAside={updatePulledAside}
       />
       <VideoCallStudentThumbnail
-        src={PlaceholderImage}
+        src={S3}
         idx={3}
         pulledAside={pulledAside}
         updatePullAside={updatePulledAside}
       />
       <VideoCallStudentThumbnail
-        src={PlaceholderImage}
+        src={S4}
         idx={4}
         pulledAside={pulledAside}
         updatePullAside={updatePulledAside}
       />
       <VideoCallStudentThumbnail
-        src={PlaceholderImage}
+        src={S6}
         idx={5}
         pulledAside={pulledAside}
         updatePullAside={updatePulledAside}
       />
       <VideoCallStudentThumbnail
-        src={PlaceholderImage}
+        src={S5}
         idx={6}
         pulledAside={pulledAside}
         updatePullAside={updatePulledAside}
       />
       <VideoCallStudentThumbnail
-        src={PlaceholderImage}
+        src={S1}
         idx={7}
         pulledAside={pulledAside}
         updatePullAside={updatePulledAside}
       />
       <VideoCallStudentThumbnail
-        src={PlaceholderImage}
+        src={S2}
         idx={8}
         pulledAside={pulledAside}
         updatePullAside={updatePulledAside}
