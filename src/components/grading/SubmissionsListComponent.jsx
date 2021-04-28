b234993720cc5530639e592df6092b6e587453a8
daniaalnahdi <alnahdi.d@husky.neu.edu>
2021-04-28 21:12:37 +0300

    update grading list screen

--- src/components/grading/SubmissionsListComponent.jsx
+++ src/components/grading/SubmissionsListComponent.jsx
@@ -3,31 +3,50 @@ import { Grid } from '@material-ui/core';
 
 import SubmissionRowComponent from './SubmissionRowComponent';
 
+import GradingSubmissionsHeader from '../../assets/GradingSubmissionsHeader';
+
 const SubmissionsListComponent = (props) => {
   return (
     <div className='section-container'>
-      <Grid container direction='row' spacing={3}>
-        <Grid item xs={10}>
-          <div className='section-title'>
-            All Submissions
-          </div>
-        </Grid>
-        <Grid item xs={2}>
-          <div className="textfont">Sort By</div>
-        </Grid>
-      </Grid>
+      <GradingSubmissionsHeader />
       <Grid container direction='row' spacing={3}>
         <Grid item xs={5}>
-          <div className="textfont">Title</div>
+          <div className='textfont'>Title</div>
         </Grid>
         <Grid item xs={3}>
-          <div className="textfont">Assigned</div>
+          <div className='textfont'>Assigned</div>
         </Grid>
         <Grid item xs={4}>
-          <div className="textfont">Submitted</div>
+          <div className='textfont'>Submitted</div>
         </Grid>
       </Grid>
-      <SubmissionRowComponent {...props} />
+      <SubmissionRowComponent
+        {...props}
+        emoji='🔢'
+        title='Addition and Subtraction'
+        subtitle='Practicing our work from class today'
+        date='Mar 17'
+        submitted='26'
+        tooltip={true}
+      />
+      <SubmissionRowComponent
+        {...props}
+        emoji='🐟'
+        title='Addition Under the Sea'
+        subtitle='Practice two digit addition under the sea w..'
+        date='Mar 17'
+        submitted='27'
+        tooltip={false}
+      />
+      <SubmissionRowComponent
+        {...props}
+        emoji='🧸'
+        title={`Barry's Place Value`}
+        subtitle='Help Barry figure out tens from ones by wri..'
+        date='Mar 17'
+        submitted='26'
+        tooltip={false}
+      />
     </div>
   );
 };
